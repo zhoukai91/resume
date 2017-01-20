0ba65ab18f8115d3f7f790dad9d23c8619062d31
zhoukai91 <you@example.com276203876@qq.com>
2017-01-20 09:48:31 +0800

    commit 2017-1-20

--- assets/index/js/index.js
+++ assets/index/js/index.js
@@ -6,7 +6,12 @@ $(function () {
   $sectionBody.sectionWrapper({
     "showSerial": false
   });
-  $(function () {
-    $('#myTab').tab('show')
-  })
+  //页签切换JS代码
+  $('#myTab a').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show')
+  });
+/*  var option = {};
+  $('#myModal').modal(options);*/
 });
+
