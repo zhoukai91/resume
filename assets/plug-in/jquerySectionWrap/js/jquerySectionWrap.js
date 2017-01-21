d60fb44313409182124b2e76f936d08abebcf74b
zhoukai91 <you@example.com276203876@qq.com>
2017-01-21 19:36:14 +0800

    commit 2017-1-21

--- assets/plug-in/jquerySectionWrap/js/jquerySectionWrap.js
+++ assets/plug-in/jquerySectionWrap/js/jquerySectionWrap.js
@@ -34,12 +34,34 @@
 			this.trigger(event,args);
 		},
 		_bind : function(){
-			var $links = $('div#tosection a');
-			var $self = this;//
-			this.on("mousewheel",function(e){
-				if($self.lock&&($(document.body).width()>768)){
+			var $links = $('div#tosection a'),
+				$self = this,//
+				startX = null,
+				startY = null,
+				moveEndX = null,
+				moveEndY = null,
+				myCss = function(index){
+						return {
+							"transform": "translateY(-"+index+"00%)",
+							"-moz-transform": "translateY(-"+index+"00%)",
+							"-webkit-transform": "translateY(-"+index+"00%)",
+							"-o-transform": "translateY(-"+index+"00%)"
+						}
+					};
+
+			this.on("touchstart",function(e){
+				e.preventDefault();
+				startX = e.originalEvent.changedTouches[0].pageX;
+				startY = e.originalEvent.changedTouches[0].pageY;
+			});
+			this.on("touchmove",function(e){
+				e.preventDefault();
+				moveEndX = e.originalEvent.changedTouches[0].pageX;
+				moveEndY = e.originalEvent.changedTouches[0].pageY;
+
+				if($self.lock){
 					$self.lock = false;
-					var dir = e.originalEvent.deltaY<0;
+					var dir = (moveEndY - startY)>0; //dir > 0 ,向上滑动
 					var beforeIndex = $self.index;
 					dir?$self.index--:$self.index++;
 					$self.index = Math.min($self.index,$self.last);
@@ -54,12 +76,7 @@
 						after : $self.index,
 						afterDOM : $self.$sections.eq($self.index)
 					});
-					$self.$sectionWrap.css({
-						"transform": "translateY(-"+$self.index+"00%)",
-						"-moz-transform": "translateY(-"+$self.index+"00%)",
-						"-webkit-transform": "translateY(-"+$self.index+"00%)",
-						"-o-transform": "translateY(-"+$self.index+"00%)"
-					});
+					$self.$sectionWrap.css(myCss($self.index));
 					setTimeout(function(){
 						$self.lock = true;
 						$self._attachEvent("afterWheel",{
@@ -78,17 +95,49 @@
 					},1000);
 				}
 			});
+			this.on("mousewheel",function(e){
+					e.preventDefault();
+					if($self.lock){
+						$self.lock = false;
+						var dir = e.originalEvent.deltaY<0;
+						var beforeIndex = $self.index;
+						dir?$self.index--:$self.index++;
+						$self.index = Math.min($self.index,$self.last);
+						$self.index = Math.max($self.index,0);
+						if(beforeIndex==$self.index){
+							$self.lock = true;
+							return;
+						}
+						$self._attachEvent("beforeWheel",{
+							before : beforeIndex,
+							beforeDOM : $self.$sections.eq(beforeIndex),
+							after : $self.index,
+							afterDOM : $self.$sections.eq($self.index)
+						});
+						$self.$sectionWrap.css(myCss($self.index));
+						setTimeout(function(){
+							$self.lock = true;
+							$self._attachEvent("afterWheel",{
+								before : beforeIndex,
+								beforeDOM : $self.$sections.eq(beforeIndex),
+								after : $self.index,
+								afterDOM : $self.$sections.eq($self.index)
+							});
+							this.showSerial&&
+									$self.$serials
+										.children()
+										.eq($self.index)
+										.addClass("curr")
+										.siblings()
+										.removeClass("curr");
+						},1000);
+					}
+			});
 			$links.on('click',function(){
-				if($(document.body).width()<768) return true;
 				index = $(this).data('jq-section');
 				$self.index = index;
-				console.log($self.index);
-				$self.$sectionWrap.css({
-					"transform": "translateY(-"+$self.index+"00%)",
-					"-moz-transform": "translateY(-"+$self.index+"00%)",
-					"-webkit-transform": "translateY(-"+$self.index+"00%)",
-					"-o-transform": "translateY(-"+$self.index+"00%)"
-				});
+				// console.log($self.index);
+				$self.$sectionWrap.css(myCss($self.index));
 				return false;
 			});
 		}
